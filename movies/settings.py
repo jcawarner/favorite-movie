6e7d615e0fe5ab027b36238741e5789e3d2aa18f
Jake Warner <jcaprogamming@gmail.com>
2022-01-20 10:16:41 -0700

    fixed decouple bug in settings.py

--- movies/settings.py
+++ movies/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-from decopule import config  
+from decouple import config  
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
