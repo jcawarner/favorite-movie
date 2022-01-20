b77152fc2f3e42e65b024967a730a6e4bde18a94
Jake Warner <jcaprogamming@gmail.com>
2022-01-20 10:14:14 -0700

    fixed error in settings

--- movies/settings.py
+++ movies/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import djanog_heroku
+import django_heroku
 from decopule import config  
 import dj_database_url
 
